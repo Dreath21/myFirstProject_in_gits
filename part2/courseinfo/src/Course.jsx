e975efa3d86485e791c6c092f557a284afeb6eb8
Dreath21 <sinevasankrishloren@gmail.com>
2025-02-19 18:36:40 +0800

    courseinfo of part2 done (#6)

---
+++ part2/courseinfo/src/Course.jsx
@@ -0,0 +1,29 @@
+const Course = ({ course }) => {
+    const { name, parts } = course
+    console.log("The Name: ", name, "The parts: ", parts);
+  
+    return (
+      <>
+        <Header name={name} />
+        <Content parts={parts} />
+        Total of {parts.map(part => part.exercises).reduce((a, p) => a + p, 0)} exercises
+      </>
+    )
+  }
+  
+  const Header = ({ name }) => {
+    console.log("Header", name);
+    return (
+      <h1>{name}</h1>
+    )
+  }
+  
+  const Content = ({ parts }) => {
+    console.log("Contents parts ", parts);
+    return (
+      <>
+        {parts.map(part => <li key={part.id}>{part.name} {part.exercises}</li>)}
+      </>
+    )
+  }
+  export default Course
